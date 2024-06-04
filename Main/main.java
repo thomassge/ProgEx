379683c1fb44f2aefb992d7275ab76646753f0fa
Tim Bornemann <65897899+timbornemann@users.noreply.github.com>
2024-06-04 16:37:14 +0200

    Füge unsere Namen zur Main hinzu

--- Main/main.java
+++ Main/main.java
@@ -6,6 +6,9 @@ import javax.swing.*;
 
 public class main {
 
+//Programming Exercises Project 2024 
+//Group members who participated in creating the Project: Sara Abassi, Tim Bornemann, Eyüp Korkmaz, Thomas Levantis, Annika Zoe Schäffler, Finn Schäffler
+
     public static void main(String[] args) {
         Manager manager = new Manager();
         SwingUtilities.invokeLater(() -> new MainGui());
